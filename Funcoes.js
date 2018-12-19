583c3178a4154227b3a5b563d0afc58165db431f
InacioJunior10 <inaciojunior10@hotmail.com>
2018-12-18 23:56:51 -0200

    Final dos Testes

--- Funcoes.js
+++ Funcoes.js
@@ -1,3 +1,4 @@
+//#region Baralho
 var naipes = ["C", "E", "O", "P"];
 var numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
@@ -27,3 +28,47 @@ function embaralhar(baralho){
 
     return embaralhado;
 }
+//#endregion
+
+//#region Hora
+var FakeHora = function(_hora){
+    this.hora = _hora;   
+    
+    this.getHora = function(){
+        return this.hora;
+    };
+
+
+}
+//#endregion
+
+//#region Baralho Viciado
+var BaralhoViciado = function(_hora){
+    this.cartas = [];
+
+    this.montarBaralho = function(){
+        naipes.forEach(naipe => {
+            numeros.forEach(numero => {
+                this.cartas.push(naipe + numero);
+            });
+        });    
+    };
+
+    this.montarBaralho();
+
+    if (_hora < 12){
+        return this;
+    }else{
+        return embaralhar(this);
+    }
+};
+//#endregion
+
+//#region Jogador
+var Jogador = function(){
+    this.darCarta = function(baralho){
+        return baralho.cartas.splice(0, 1)    
+    }
+};
+//#endregion
+
--- Testes.js
+++ Testes.js
@@ -1,3 +1,4 @@
+//#region Testes
 function testeMontarBaralho(){
     var baralho = new Baralho();
 
@@ -27,9 +28,80 @@ function testeEmbaralhar(){
         console.log("Sucesso - testeEmbaralhar");
     }else{
         console.log("Erro - testeEmbaralhar");
+    }    
+}
+
+function testeAntesMeioDia(){
+    var h = new FakeHora(11);
+    
+    if (h.getHora().toString() < 12){
+        console.log("Sucesso - testeAntesMeioDia");
+    }else{
+        console.log("Erro - testeAntesMeioDia");
     }
 }
 
+function testeDepoisMeioDia(){
+    var h = new FakeHora(13);
+    
+    if (h.getHora().toString() > 12){
+        console.log("Sucesso - testeDepoisMeioDia");
+    }else{
+        console.log("Erro - testeDepoisMeioDia");
+    }
+}
+
+function testeBaralhoViciadoAntesMeioDia(){
+    var viciado = new BaralhoViciado(11);
+
+    if (viciado.cartas.toString() == "CA,C2,C3,C4,C5,C6,C7,C8,C9,C10,CJ,CQ,CK,EA,E2,E3,E4,E5,E6,E7,E8,E9,E10,EJ,EQ,EK,OA,O2,O3,O4,O5,O6,O7,O8,O9,O10,OJ,OQ,OK,PA,P2,P3,P4,P5,P6,P7,P8,P9,P10,PJ,PQ,PK"){
+        console.log("Sucesso - testeBaralhoViciadoAntesMeioDia");
+    }else{
+        console.log("Erro - testeBaralhoViciadoAntesMeioDia");
+    }
+}
+
+function testeBaralhoViciadoDepoisMeioDia(){
+    var baralho = new BaralhoViciado(11);
+    var viciado = new BaralhoViciado(13);
+
+    if (baralho.cartas.toString() != viciado.cartas.toString()){
+        console.log("Sucesso - testeBaralhoViciadoDepoisMeioDia");
+    }else{
+        console.log("Erro - testeBaralhoViciadoDepoisMeioDia");
+    }
+}
+
+function testeDarCarta(){
+    var baralho = new Baralho();
+    var totalAntes = baralho.cartas.length;
+
+    var jogador = new Jogador();
+    var carta = jogador.darCarta(baralho);
+
+    var totalDepois = baralho.cartas.length;
+
+    if (totalDepois == (totalAntes -1)){
+        console.log("Sucesso - testeDarCarta");
+    }else{
+        console.log("Erro - testeDarCarta");
+    }    
+
+    if (carta == "CA"){
+        console.log("Sucesso - testeDarCarta - Primeira Carta");
+    }else{
+        console.log("Erro - testeDarCarta - Primeira Carta");
+    }
+}
+//#endregion
+
+//#region Chamadas dos Testes
 testeMontarBaralho();
 testeOrdenar();
 testeEmbaralhar();
+testeAntesMeioDia();
+testeDepoisMeioDia();
+testeBaralhoViciadoAntesMeioDia();
+testeBaralhoViciadoDepoisMeioDia();
+testeDarCarta();
+//#endregion
